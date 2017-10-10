a161c6ae9b5898b4d2c02544d964fe04e2a32770
mmarcec007 <mark.marcec@gmail.com>
2017-10-10 20:03:49 +0200

    [TASK] add core dao and instantiate other daos from it

---
+++ app/src/main/java/com/example/mark/mydoctors/dao/CoreDao.java
@@ -0,0 +1,35 @@
+package com.example.mark.mydoctors.dao;
+
+import android.content.Context;
+
+import com.example.mark.mydoctors.DatabaseOperations.DBHelper;
+
+/**
+ * Created by Mark on 10.10.2017..
+ */
+
+public class CoreDao extends DBHelper {
+
+    Context contextField;
+
+    public CoreDao(Context context)
+    {
+        super(context);
+        contextField = context;
+    }
+
+    public PatientDao instantiatePatientDao()
+    {
+        return new PatientDao(contextField);
+    }
+
+    public DiseaseDao instantiateDiseaseDao()
+    {
+        return new DiseaseDao(contextField);
+    }
+
+    public MedicineDao instantiateMedicinenDao()
+    {
+        return new MedicineDao(contextField);
+    }
+}
